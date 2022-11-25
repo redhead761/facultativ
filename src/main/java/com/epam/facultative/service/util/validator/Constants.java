b1eb5af69806860c3c1dcb960b0d7d842a92e1af
Oleksandr Panchenko <a.panchenko761@gmail.com>
2022-11-25 13:46:59 +0200

    refactoring validator

---
+++ src/main/java/com/epam/facultative/service/util/validator/Constants.java
@@ -0,0 +1,27 @@
+package com.epam.facultative.service.util.validator;
+
+class Constants {
+    //Regex
+    static final String TITLE_PATTERN_REGEX = "^[A-Za-zА-ЩЬЮЯҐІЇЄа-щьюяґіїє'\\- 0-9]{1,30}";
+    static final String EMAIL_PATTERN_REGEX = "^[\\w.%+-]+@[\\w.-]+\\.[a-zA-Z]{2,6}$";
+    static final String LOGIN_PATTERN_REGEX = "([a-zA-Z0-9]+([_ -]?[a-zA-Z0-9])*){5,16}";
+    static final String PASSWORD_PATTERN_REGEX = "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=\\S+$).{8,20}$";
+    static final String NAME_PATTERN_REGEX = "^[A-Za-zА-ЩЬЮЯҐІЇЄа-щьюяґіїє'\\- ]{1,30}";
+
+    static final String DESCRIPTION_PATTERN_REGEX = "^[\\wА-ЩЬЮЯҐІЇЄа-щьюяґіїє'.,;:+\\-~`!@#$^&*()={} ]{0,200}";
+
+    //Exception messages
+    static final String LOGIN_EXCEPTION_MESSAGE = "You entered incorrect login, login must ....";
+
+    static final String PASSWORD_EXCEPTION_MESSAGE = "You entered incorrect password, password must...";
+
+    static final String NAME_EXCEPTION_MESSAGE = "You entered incorrect name or surname, name or surname must ...";
+
+    static final String EMAIL_EXCEPTION_MESSAGE = "You entered incorrect email, email must ....";
+
+    static final String CATEGORY_DATA_EXCEPTION_MESSAGE = "You entered incorrect category data...";
+
+    static final String COURSE_DATA_EXCEPTION_MESSAGE = "You entered incorrect course data...";
+
+
+}
--- src/main/java/com/epam/facultative/service/util/validator/Validator.java
+++ src/main/java/com/epam/facultative/service/util/validator/Validator.java
@@ -1,4 +1,53 @@
 package com.epam.facultative.service.util.validator;
 
-public interface Validator {
+import com.epam.facultative.exception.ValidateException;
+
+import static com.epam.facultative.service.util.validator.Constants.*;
+
+public final class Validator {
+
+    private Validator() {
+    }
+
+    public static boolean validateLogin(String login) throws ValidateException {
+        if (!login.matches(LOGIN_PATTERN_REGEX)) {
+            throw new ValidateException(LOGIN_EXCEPTION_MESSAGE);
+        }
+        return true;
+    }
+
+    private static boolean validatePassword(String password) throws ValidateException {
+        if (!password.matches(PASSWORD_PATTERN_REGEX)) {
+            throw new ValidateException(PASSWORD_EXCEPTION_MESSAGE);
+        }
+        return true;
+    }
+
+    private static boolean validateEmail(String email) throws ValidateException {
+        if (!email.matches(EMAIL_PATTERN_REGEX)) {
+            throw new ValidateException(EMAIL_EXCEPTION_MESSAGE);
+        }
+        return true;
+    }
+
+    private static boolean validateNameAndSurname(String name, String surname) throws ValidateException {
+        if (!name.matches(NAME_PATTERN_REGEX) || !surname.matches(NAME_PATTERN_REGEX)) {
+            throw new ValidateException(NAME_EXCEPTION_MESSAGE);
+        }
+        return true;
+    }
+
+    private static boolean validateCourseData(String title, String description) throws ValidateException {
+        if (!title.matches(TITLE_PATTERN_REGEX) || !description.matches(DESCRIPTION_PATTERN_REGEX)) {
+            throw new ValidateException(CATEGORY_DATA_EXCEPTION_MESSAGE);
+        }
+        return true;
+    }
+
+    private static boolean validateCourseData(String title, String description, int duration) throws ValidateException {
+        if (!title.matches(TITLE_PATTERN_REGEX) || !description.matches(DESCRIPTION_PATTERN_REGEX) || duration <= 0) {
+            throw new ValidateException(COURSE_DATA_EXCEPTION_MESSAGE);
+        }
+        return true;
+    }
 }

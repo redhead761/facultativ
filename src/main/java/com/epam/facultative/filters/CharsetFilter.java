71fddd7cc84ca094ca86ed858dfb8d4a6504c80a
Oleksandr Panchenko <a.panchenko761@gmail.com>
2022-12-20 17:13:35 +0200

    added charset filter

---
+++ src/main/java/com/epam/facultative/filters/CharsetFilter.java
@@ -0,0 +1,17 @@
+package com.epam.facultative.filters;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+
+import java.io.IOException;
+
+@WebFilter("/*")
+public class CharsetFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        servletRequest.setCharacterEncoding("UTF-8");
+        servletResponse.setContentType("text/html; charset=UTF-8");
+        servletResponse.setCharacterEncoding("UTF-8");
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+}

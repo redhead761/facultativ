d2dbc6fdde32dfc3868d1f32d0fbba0384b4090c
Oleksandr Panchenko <a.panchenko761@gmail.com>
2022-12-21 19:04:55 +0200

    refactor fields for DB

--- src/main/java/com/epam/facultative/daos/impl/Fields.java
+++ src/main/java/com/epam/facultative/daos/impl/Fields.java
@@ -18,20 +18,33 @@ class Fields {
     static final String COURSE_TITLE = "course.title";
     static final String COURSE_DURATION = "course.duration";
     static final String COURSE_START_DATE = "course.start_date";
-    static final String COURSE_AMOUNT_STUDENTS = "course.amount_students";
     static final String COURSE_DESCRIPTION = "course.description";
-    static final String CATEGORY_ID = "category.id";
-    static final String COURSE_STATUS = "status.title";
-    static final String COURSE_GRADE = "grade";
 
     //User fields
     static final String USER_ID = "user.id";
-    static final String USER_LOGIN = "login";
-    static final String USER_PASSWORD = "password";
-    static final String USER_FIRST_NAME = "name";
-    static final String USER_FAMILY_NAME = "surname";
-    static final String USER_EMAIL = "email";
-    static final String USER_IS_BLOCK = "block";
-    static final String USER_ROLE_NAME = "role_name";
+    static final String USER_LOGIN = "user.login";
+    static final String USER_PASSWORD = "user.password";
+    static final String USER_FIRST_NAME = "user.name";
+    static final String USER_FAMILY_NAME = "user.surname";
+    static final String USER_EMAIL = "user.email";
 
+    //Teacher fields
+    static final String TEACHER_ID = "teacher.id";
+    static final String TEACHER_RANK = "teacher.rank";
+
+    //Student fields
+    static final String STUDENT_ID = "student.id";
+    static final String STUDENT_COURSE_NUMBER = "student.course_number";
+    static final String STUDENT_BLOCK = "student.block";
+
+    //Role fields
+    static final String ROLE_ID = "role.id";
+    static final String ROLE_NAME = "role.name";
+
+    //Status fields
+    static final String STATUS_ID = "status.id";
+    static final String STATUS_TITLE = "status.title";
+
+    //Journal fields
+    static final String GRADE = "journal.grade";
 }

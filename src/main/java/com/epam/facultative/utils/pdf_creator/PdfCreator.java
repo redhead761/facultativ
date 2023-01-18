f5613025610e577f65f0ac588002f8fbbae4603c
Oleksandr Panchenko <a.panchenko761@gmail.com>
2023-01-18 14:24:03 +0200

    added download certificate

--- src/main/java/com/epam/facultative/utils/pdf_creator/PdfCreator.java
+++ src/main/java/com/epam/facultative/utils/pdf_creator/PdfCreator.java
@@ -1,6 +1,10 @@
 package com.epam.facultative.utils.pdf_creator;
 
+import com.epam.facultative.data_layer.entities.Course;
 import com.epam.facultative.dto.CourseDTO;
+import com.epam.facultative.dto.StudentDTO;
+import com.itextpdf.io.image.ImageData;
+import com.itextpdf.io.image.ImageDataFactory;
 import com.itextpdf.kernel.colors.Color;
 import com.itextpdf.kernel.colors.DeviceRgb;
 import com.itextpdf.kernel.font.PdfFont;
@@ -10,10 +14,8 @@ import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfWriter;
 import com.itextpdf.layout.Document;
 import com.itextpdf.layout.borders.SolidBorder;
-import com.itextpdf.layout.element.Cell;
-import com.itextpdf.layout.element.Paragraph;
-import com.itextpdf.layout.element.Table;
-import com.itextpdf.layout.element.Text;
+import com.itextpdf.layout.element.*;
+import com.itextpdf.layout.properties.BackgroundImage;
 import com.itextpdf.layout.properties.TextAlignment;
 import com.itextpdf.layout.properties.UnitValue;
 import org.slf4j.Logger;
@@ -21,8 +23,10 @@ import org.slf4j.LoggerFactory;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.util.List;
 import java.util.Locale;
+import java.util.Optional;
 import java.util.ResourceBundle;
 import java.util.stream.Stream;
 
@@ -30,13 +34,13 @@ import static java.util.ResourceBundle.getBundle;
 
 public class PdfCreator {
     private static final Logger logger = LoggerFactory.getLogger(PdfCreator.class);
-
     private static final String COURSE_TITLE = "all.course";
     private static final int TITLE_SIZE = 20;
     private static final Paragraph LINE_SEPARATOR = new Paragraph(new Text("\n"));
     private static final Color LIGHT_GREY = new DeviceRgb(220, 220, 220);
     private static final String[] COURSE_CELLS =
             new String[]{"title", "duration", "start.date", "amount.students", "category", "status", "teacher"};
+    private static final String PATH_FONT = "C:\\epam\\facultative\\pdf\\arial.ttf";
 
     public ByteArrayOutputStream createCoursesPdf(List<CourseDTO> courses, String locale) {
         ResourceBundle currentResourceBundle = getCurrentResourceBundle(locale);
@@ -49,16 +53,34 @@ public class PdfCreator {
         return output;
     }
 
+    public ByteArrayOutputStream createCertificate(StudentDTO studentDTO, Course course, int grade) {
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        Document document = getPdfDocument(output);
+        document.add(new Paragraph(new Text("Certificate"))
+                .setFontSize(TITLE_SIZE)
+                .setTextAlignment(TextAlignment.CENTER));
+        document.add(LINE_SEPARATOR);
+        document.add(new Paragraph(new Text("Congratulation!" + studentDTO.getName() + " " + studentDTO.getSurname())));
+        document.add(LINE_SEPARATOR);
+        document.add(new Paragraph(new Text("You have successfully completed the course: " + course.getTitle())));
+        document.add(LINE_SEPARATOR);
+        document.add(new Paragraph(new Text("Your grade: " + grade)));
+        document.add(LINE_SEPARATOR);
+        document.add(new Paragraph(new Text("Hours listened: " + course.getDuration())));
+        document.close();
+        return output;
+    }
+
     private Table getCoursesTable(List<CourseDTO> courses, ResourceBundle resourceBundle) {
         Table table = new Table(new float[]{7, 3, 3, 2, 2, 2, 5});
         table.setWidth(UnitValue.createPercentValue(100));
-        addTableHeader(table, COURSE_CELLS, resourceBundle);
+        addTableHeader(table, resourceBundle);
         addCoursesTableRows(table, courses);
         return table;
     }
 
-    private void addTableHeader(Table table, String[] cells, ResourceBundle resourceBundle) {
-        Stream.of(cells)
+    private void addTableHeader(Table table, ResourceBundle resourceBundle) {
+        Stream.of(COURSE_CELLS)
                 .forEach(columnTitle -> {
                     Cell header = new Cell();
                     header.setBackgroundColor(LIGHT_GREY);
@@ -86,7 +108,7 @@ public class PdfCreator {
         );
     }
 
-    private static Paragraph getTableTitle(String tableTitle) {
+    private Paragraph getTableTitle(String tableTitle) {
         return new Paragraph(new Text(tableTitle))
                 .setFontSize(TITLE_SIZE)
                 .setTextAlignment(TextAlignment.CENTER);
@@ -105,7 +127,7 @@ public class PdfCreator {
 
     private PdfFont getPdfFont() {
         try {
-            return PdfFontFactory.createFont("C:\\epam\\facultative\\pdf\\arial.ttf");
+            return PdfFontFactory.createFont(PATH_FONT);
         } catch (IOException e) {
             logger.error(e.getMessage());
             return null;
@@ -114,9 +136,6 @@ public class PdfCreator {
 
     private ResourceBundle getCurrentResourceBundle(String locale) {
         String resources = "resources";
-//        if (locale == null) {
-//            return getBundle(resources, new Locale("en"));
-//        }
         if (locale.contains("_")) {
             String[] splitLocale = locale.split("_");
             return getBundle(resources, new Locale(splitLocale[0], splitLocale[1]));

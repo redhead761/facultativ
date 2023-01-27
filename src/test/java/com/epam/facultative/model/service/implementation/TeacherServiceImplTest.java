30148c75af70b6f29313518f9f0490471cdf4850
Oleksandr Panchenko <a.panchenko761@gmail.com>
2023-01-27 23:18:11 +0200

    refactoring teacher service tests

--- src/test/java/com/epam/facultative/model/service/implementation/TeacherServiceImplTest.java
+++ src/test/java/com/epam/facultative/model/service/implementation/TeacherServiceImplTest.java
@@ -3,15 +3,26 @@ package com.epam.facultative.model.service.implementation;
 import com.epam.facultative.model.dao.CourseDao;
 import com.epam.facultative.model.dao.StudentDao;
 import com.epam.facultative.model.dao.TeacherDao;
+import com.epam.facultative.model.dto.StudentDTO;
+import com.epam.facultative.model.dto.TeacherDTO;
+import com.epam.facultative.model.entities.Student;
+import com.epam.facultative.model.entities.Teacher;
 import com.epam.facultative.model.exception.DAOException;
 import com.epam.facultative.model.exception.ServiceException;
+import com.epam.facultative.model.exception.ValidateException;
 import com.epam.facultative.model.service.TeacherService;
+import com.epam.facultative.model.utils.converter.Converter;
+import com.epam.facultative.model.utils.validator.Validator;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.MockedStatic;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -24,15 +35,23 @@ class TeacherServiceImplTest {
     private final StudentDao studentDao = mock(StudentDao.class);
     private final TeacherDao teacherDao = mock(TeacherDao.class);
     TeacherService teacherService = new TeacherServiceImpl(courseDao, studentDao, teacherDao);
-    private static TestServiceUtil testServiceUtil;
+    private final String TEST_PARAM = "TEST";
+    private static Map.Entry<Integer, List<Student>> students;
+    private static Map.Entry<Integer, List<StudentDTO>> studentDTOS;
+    private static Teacher teacher;
+    private static TeacherDTO teacherDTO;
 
     @BeforeAll
     static void beforeAll() {
-        testServiceUtil = new TestServiceUtil();
+        TestServiceUtil testServiceUtil = new TestServiceUtil();
+        students = testServiceUtil.getStudents();
+        studentDTOS = testServiceUtil.getStudentDTOS();
+        teacher = testServiceUtil.getTeacher();
+        teacherDTO = testServiceUtil.getTeacherDTO();
     }
 
     @Test
-    void grading() throws DAOException {
+    void gradingSuccessful() throws DAOException {
         doNothing().when(courseDao).updateJournal(isA(int.class), isA(int.class), isA(int.class));
         assertDoesNotThrow(() -> teacherService.grading(1, 1, 1));
     }
@@ -44,35 +63,107 @@ class TeacherServiceImplTest {
         assertThrows(ServiceException.class, () -> teacherService.grading(courseId, userId, grade));
     }
 
-//    @Test
-//    void getStudentsByCourse() throws DAOException, ServiceException {
-//        List<Student> students = testServiceUtil.getStudents();
-//        List<StudentDTO> studentDTOS = testServiceUtil.getStudentDTOS();
-//        when(studentDao.getStudentsByCourse(isA(int.class), isA(int.class), isA(int.class))).thenReturn(students);
-//        assertIterableEquals(studentDTOS, teacherService.getStudentsByCourse(1, 1, 5));
-//    }
+    @Test
+    void getStudentsByCourseSuccessful() throws DAOException, ServiceException {
+        try (MockedStatic<Converter> converter = mockStatic(Converter.class)) {
+            when(studentDao.getStudentsByCourse(anyString())).thenReturn(students);
+            converter.when(() -> Converter.prepareStudents(students.getValue())).thenReturn(studentDTOS.getValue());
+            assertEquals(studentDTOS, teacherService.getStudentsByCourse(TEST_PARAM));
+        }
+    }
 
-//    @ParameterizedTest
-//    @MethodSource("invalidIntValues")
-//    void getStudentsByCourseWithIllegalArgument(int courseId, int offset, int numberOfRows) throws DAOException {
-//        doThrow(DAOException.class).when(studentDao).getStudentsByCourse(isA(int.class), isA(int.class), isA(int.class));
-//        assertThrows(ServiceException.class, () -> teacherService.getStudentsByCourse(courseId, offset, numberOfRows));
-//    }
+    @Test
+    void getStudentsByCourseFailed() throws DAOException {
+        try (MockedStatic<Converter> converter = mockStatic(Converter.class)) {
+            when(studentDao.getStudentsByCourse(anyString())).thenThrow(DAOException.class);
+            converter.when(() -> Converter.prepareStudents(students.getValue())).thenReturn(studentDTOS.getValue());
+            assertThrows(ServiceException.class, () -> teacherService.getStudentsByCourse(TEST_PARAM));
+        }
+    }
 
-//    @Test
-//    void getTeacherCourses() throws DAOException, ServiceException {
-//        List<Course> courses = testServiceUtil.getCourses();
-//        List<CourseDTO> courseDTOS = testServiceUtil.getTCourseDTOS();
-//        when(courseDao.getByTeacher(isA(int.class), isA(int.class), isA(int.class))).thenReturn(courses);
-//        assertIterableEquals(courseDTOS, teacherService.getTeacherCourses(1, 1, 5));
-//    }
+    @Test
+    void updateStudentSuccessful() throws DAOException, ValidateException {
+        try (MockedStatic<Validator> validator = mockStatic(Validator.class);
+             MockedStatic<Converter> converter = mockStatic(Converter.class)) {
+            validator.when(() -> Validator.validateLogin(anyString())).thenAnswer(invocation -> null);
+            validator.when(() -> Validator.validateName(anyString())).thenAnswer(invocation -> null);
+            validator.when(() -> Validator.validateEmail(anyString())).thenAnswer(invocation -> null);
+            converter.when(() -> Converter.convertDTOToTeacher(teacherDTO)).thenReturn(teacher);
+            when(teacherDao.get(anyString())).thenReturn(Optional.ofNullable(teacher));
+            doNothing().when(teacherDao).update(teacher);
+            assertDoesNotThrow(() -> teacherService.updateTeacher(teacherDTO));
+        }
+    }
 
-//    @ParameterizedTest
-//    @MethodSource("invalidIntValues")
-//    void getTeacherCoursesWithIllegalArgument(int courseId, int offset, int numberOfRows) throws DAOException {
-//        doThrow(DAOException.class).when(courseDao).getByTeacher(isA(int.class), isA(int.class), isA(int.class));
-//        assertThrows(ServiceException.class, () -> teacherService.getTeacherCourses(courseId, offset, numberOfRows));
-//    }
+    @Test
+    void updateStudentWithInvalidLogin() throws DAOException, ValidateException {
+        try (MockedStatic<Validator> validator = mockStatic(Validator.class);
+             MockedStatic<Converter> converter = mockStatic(Converter.class)) {
+            validator.when(() -> Validator.validateLogin(anyString())).thenThrow(ValidateException.class);
+            validator.when(() -> Validator.validateName(anyString())).thenAnswer(invocation -> null);
+            validator.when(() -> Validator.validateEmail(anyString())).thenAnswer(invocation -> null);
+            converter.when(() -> Converter.convertDTOToTeacher(teacherDTO)).thenReturn(teacher);
+            when(teacherDao.get(anyString())).thenReturn(Optional.ofNullable(teacher));
+            doNothing().when(teacherDao).update(teacher);
+            assertThrows(ValidateException.class, () -> teacherService.updateTeacher(teacherDTO));
+        }
+    }
+
+    @Test
+    void updateStudentWithInvalidName() throws DAOException, ValidateException {
+        try (MockedStatic<Validator> validator = mockStatic(Validator.class);
+             MockedStatic<Converter> converter = mockStatic(Converter.class)) {
+            validator.when(() -> Validator.validateLogin(anyString())).thenAnswer(invocation -> null);
+            validator.when(() -> Validator.validateName(anyString())).thenThrow(ValidateException.class);
+            validator.when(() -> Validator.validateEmail(anyString())).thenAnswer(invocation -> null);
+            converter.when(() -> Converter.convertDTOToTeacher(teacherDTO)).thenReturn(teacher);
+            when(teacherDao.get(anyString())).thenReturn(Optional.ofNullable(teacher));
+            doNothing().when(teacherDao).update(teacher);
+            assertThrows(ValidateException.class, () -> teacherService.updateTeacher(teacherDTO));
+        }
+    }
+
+    @Test
+    void updateStudentWithInvalidEmail() throws DAOException, ValidateException {
+        try (MockedStatic<Validator> validator = mockStatic(Validator.class);
+             MockedStatic<Converter> converter = mockStatic(Converter.class)) {
+            validator.when(() -> Validator.validateLogin(anyString())).thenAnswer(invocation -> null);
+            validator.when(() -> Validator.validateName(anyString())).thenAnswer(invocation -> null);
+            validator.when(() -> Validator.validateEmail(anyString())).thenThrow(ValidateException.class);
+            converter.when(() -> Converter.convertDTOToTeacher(teacherDTO)).thenReturn(teacher);
+            when(teacherDao.get(anyString())).thenReturn(Optional.ofNullable(teacher));
+            doNothing().when(teacherDao).update(teacher);
+            assertThrows(ValidateException.class, () -> teacherService.updateTeacher(teacherDTO));
+        }
+    }
+
+    @Test
+    void updateStudentFailedGet() throws DAOException, ValidateException {
+        try (MockedStatic<Validator> validator = mockStatic(Validator.class);
+             MockedStatic<Converter> converter = mockStatic(Converter.class)) {
+            validator.when(() -> Validator.validateLogin(anyString())).thenAnswer(invocation -> null);
+            validator.when(() -> Validator.validateName(anyString())).thenAnswer(invocation -> null);
+            validator.when(() -> Validator.validateEmail(anyString())).thenAnswer(invocation -> null);
+            converter.when(() -> Converter.convertDTOToTeacher(teacherDTO)).thenReturn(teacher);
+            doThrow(DAOException.class).when(teacherDao).get(anyString());
+            doNothing().when(teacherDao).update(teacher);
+            assertThrows(ServiceException.class, () -> teacherService.updateTeacher(teacherDTO));
+        }
+    }
+
+    @Test
+    void updateStudentFailed() throws DAOException, ValidateException {
+        try (MockedStatic<Validator> validator = mockStatic(Validator.class);
+             MockedStatic<Converter> converter = mockStatic(Converter.class)) {
+            validator.when(() -> Validator.validateLogin(anyString())).thenAnswer(invocation -> null);
+            validator.when(() -> Validator.validateName(anyString())).thenAnswer(invocation -> null);
+            validator.when(() -> Validator.validateEmail(anyString())).thenAnswer(invocation -> null);
+            converter.when(() -> Converter.convertDTOToTeacher(teacherDTO)).thenReturn(teacher);
+            when(teacherDao.get(anyString())).thenReturn(Optional.ofNullable(teacher));
+            doThrow(DAOException.class).when(teacherDao).update(teacher);
+            assertThrows(ServiceException.class, () -> teacherService.updateTeacher(teacherDTO));
+        }
+    }
 
     private static Stream<Arguments> invalidIntValues() {
         return Stream.of(
